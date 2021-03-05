ba32ec6db151c878f4885244c2dea6a71a3a8c5b
Zachary Eggert <Eggert.Zachary@gmail.com>
2021-03-05 13:11:20 -0800

    reformat using prettier, basic readme

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Day-Planner
  
-https://zacharyeggert.github.io/Day-Planner/
+![screenshot](./gitAssets/screenshot.png)
+
+----------
+
+A simple day planner. 
+
+Built using JavaScript, jQuery, Bootstrap, and Moment.js
+
+----------
+
+[link to deployed site](https://zacharyeggert.github.io/Day-Planner/)
--- assets/js/Script.js
+++ assets/js/Script.js
@@ -5,50 +5,47 @@ const dayEl = $("#currentDay");
 const trList = tableEl.children().eq(0).children();
 
 var stored = ["", "", "", "", "", "", "", "", ""];
-if(JSON.parse(localStorage.getItem("scheduler"))===null){
-    localStorage.setItem("scheduler", JSON.stringify(stored));
-}else{
-    stored = JSON.parse(localStorage.getItem("scheduler"));
+if (JSON.parse(localStorage.getItem("scheduler")) === null) {
+  localStorage.setItem("scheduler", JSON.stringify(stored));
+} else {
+  stored = JSON.parse(localStorage.getItem("scheduler"));
 }
 
 for (let i = 0; i < trList.length; i++) {
-    //console.log(stored[i])
-    trList.eq(i).children().eq(1).children().eq(0).val(stored[i]);
+  //console.log(stored[i])
+  trList.eq(i).children().eq(1).children().eq(0).val(stored[i]);
 }
 
 saveBtn.on("click", function (e) {
-    let rowNum = parseInt($(e.target).attr("id"));
-    stored[rowNum] = trList.eq(rowNum).children().eq(1).children().eq(0).val();
-    localStorage.setItem("scheduler", JSON.stringify(stored));
-
+  let rowNum = parseInt($(e.target).attr("id"));
+  stored[rowNum] = trList.eq(rowNum).children().eq(1).children().eq(0).val();
+  localStorage.setItem("scheduler", JSON.stringify(stored));
 });
 
 revBtn.on("click", function (e) {
-    let rowNum = parseInt($(e.target).attr("id"));
-    trList.eq(rowNum).children().eq(1).children().eq(0).val(stored[rowNum]);
-    localStorage.setItem("scheduler", JSON.stringify(stored));
-
+  let rowNum = parseInt($(e.target).attr("id"));
+  trList.eq(rowNum).children().eq(1).children().eq(0).val(stored[rowNum]);
+  localStorage.setItem("scheduler", JSON.stringify(stored));
 });
 
 function updateChart() {
+  let now = moment();
+  let hour = parseInt(now.format("HH"));
 
-    let now = moment();
-    let hour = parseInt(now.format("HH"));
-
-    for (let j = 0; j < trList.length; j++) {
-        if(parseInt(trList.eq(j).attr("data-hour")) < hour){
-            trList.eq(j).attr("id", "past");
-        }else if(parseInt(trList.eq(j).attr("data-hour")) === hour){
-            trList.eq(j).attr("id", "present");
-        }else{
-            trList.eq(j).attr("id", "future");
-        }
+  for (let j = 0; j < trList.length; j++) {
+    if (parseInt(trList.eq(j).attr("data-hour")) < hour) {
+      trList.eq(j).attr("id", "past");
+    } else if (parseInt(trList.eq(j).attr("data-hour")) === hour) {
+      trList.eq(j).attr("id", "present");
+    } else {
+      trList.eq(j).attr("id", "future");
     }
+  }
 }
 
 dayEl.text("Today is " + moment().format("dddd, MMMM Do, YYYY."));
-updateChart()
+updateChart();
 
-var renderer = setInterval(function(){
-    updateChart();
+var renderer = setInterval(function () {
+  updateChart();
 }, 60000);
